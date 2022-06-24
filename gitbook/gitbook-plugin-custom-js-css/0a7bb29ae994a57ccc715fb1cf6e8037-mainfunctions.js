ce7771e99c742057a53d09517f3ffad95385473f
Norberto Agustin <norbertoam@kata-software.com>
2022-06-24 16:56:33 -0500

    actualizacion de la ruta
    actualizacion de la ruta

--- gitbook/gitbook-plugin-custom-js-css/0a7bb29ae994a57ccc715fb1cf6e8037-mainfunctions.js
+++ gitbook/gitbook-plugin-custom-js-css/0a7bb29ae994a57ccc715fb1cf6e8037-mainfunctions.js
@@ -4,7 +4,7 @@ function holaMundo(){
 
 function enviarFeedback(recurso, movimiento) {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:3002/estadisticos/EnviarEstadistica", true);
+    xhr.open("POST", "https://kata-onboarding-estadisticas.azurewebsites.net/estadisticos/EnviarEstadistica", true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.setRequestHeader('Kata', 'b7751e6a166017b5758fc8001ea381d35d7c0c82');
     xhr.setRequestHeader('Tenant', 'a4051635-1de0-4be3-b308-331daae451df');
